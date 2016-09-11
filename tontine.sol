bad770d6cb34407dfff9e501c4b9a08c57f64182
Mike Goldin <mike.goldin@consensys.net>
2016-09-11 17:27:04 +0200

    Fix compile errors

--- tontine.sol
+++ tontine.sol
@@ -1,8 +1,11 @@
+pragma solidity ^0.4.1;
+
 contract TontineOfKilpatrick {
 
     struct Nominee {
         address nomineeAddress;
-        Member[] sponsors
+        Member[] sponsors;
+        uint init;
     }
 
     struct Member {
@@ -10,6 +13,7 @@ contract TontineOfKilpatrick {
         bool isOnProbation;
         uint lastContribution;
         uint totalContribution;
+        uint init;
     }
 
     Member[] members;
@@ -25,62 +29,67 @@ contract TontineOfKilpatrick {
 
     function TontineOfKilpatrick(uint _contribution, uint _contributionInterval) {
         contribution = _contribution;
-        contributionInterbal = _contributionInterval;
-        members[members.length] = Member(msg.sender, false, now, 0);
+        contributionInterval = _contributionInterval;
+        members[members.length] = Member(msg.sender, false, now, 0, 1);
     }
 
-    function nominateMember(address nomineeAddr) is membersOnly {
-        nominees[nominees.length] = Nominee(nomineeAddr);
+    function nominateMember(address nomineeAddr) membersOnly {
+        Nominee nominee = findNominee(nomineeAddr);
+        nominees[nominees.length] = nominee;
+        nominees[nominees.length-1].nomineeAddress = nomineeAddr;
     }
 
-    function voteForNominee(address nomineeAddr) is membersOnly {
-        nominee = findNominee(nomineeAddr);
-        nominee.sponsors[nominee.sponsors.length] = msg.sender;
+    function voteForNominee(address nomineeAddr) membersOnly {
+        Nominee nominee = findNominee(nomineeAddr);
+        nominee.sponsors[nominee.sponsors.length] = findMember(msg.sender);
         if(nominee.sponsors.length > members.length/2) {
-            members[members.length] = Member(nominee.nomineeAddress, false, now, 0);
+            members[members.length] = Member(nominee.nomineeAddress, false, now, 0, 1);
         }
     }
 
-    function makePayment() is membersOnly {
-        if(msg.amount != contribution) {
+    function makePayment() membersOnly {
+        if(msg.value != contribution) {
             throw;
         }
 
-        Member member;
-        if(member = findMember(msg.sender)) {
+        Member memory member;
+        if(isMember(msg.sender)) {
+            member = findMember(msg.sender);
             member.lastContribution = now;
-            member.totalContribution += msg.amount;
+            member.totalContribution += msg.value;
         }
 
     }
 
-    function exitTontine() is membersOnly {
-        Member member = findMember(msg.sender);
+    function exitTontine() membersOnly {
+        Member memory member = findMember(msg.sender);
         member.memberAddress.call.value(member.totalContribution)();
         member.totalContribution = 0;
     }
 
-    function findMember(address addr) {
+    function findMember(address addr) internal returns (Member storage) {
         uint index = 0;
         while(index < members.length) {
             if(members[index].memberAddress == addr) {
-                return members[index];;
+                return members[index];
             }
         }
-        return false;
+        Member x;
+        return x;
     }
 
-    function findNominee(address addr) {
+    function findNominee(address addr) internal returns (Nominee storage) {
         uint index = 0;
         while(index < nominees.length) {
             if(nominees[index].nomineeAddress == addr) {
-                return nominees[index];;
+                return nominees[index];
             }
         }
-        return false;
+        Nominee x;
+        return x;
     }
 
-    function isMember(address addr) {
+    function isMember(address addr) returns (bool) {
         uint index = 0;
         while(index < members.length) {
             if(members[index].memberAddress == addr) {
